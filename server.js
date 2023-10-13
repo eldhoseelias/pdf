9a0361552bad96625d9b041c4be831e8f6f4d345
Eldhose Elias <eldhosee2@gmail.com>
2023-10-13 14:30:07 -0400

    Update server.js
    Signed-off-by: Eldhose Elias <eldhosee2@gmail.com>

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 const express = require('express');
+const puppeteer = require('puppeteer');
+
 const app = express();
-const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
+app.get('/pdf', async (req, res) => {
+    console.log('Taking screenshot');
+    console.info(process.env.CHROME_BIN,'process.env.CHROME_BIN')
+    const browser = await puppeteer.launch({
+        headless: "new",
+        executablePath: '/usr/bin/google-chrome',
+        args: ['--no-sandbox', '--headless', '--disable-gpu','--disable-setuid-sandbox']
+      });
+    const page = await browser.newPage();
+    await page.evaluate(`(async() => {
+        console.log('1');
+     })()`);
+    const html = `<!DOCTYPE html>
+    <html>
+    <body>
+    
+    <h1>My First Heading</h1>
+    
+    <p>My first paragraph.</p>
+    
+    </body>
+    </html>
+    
+    `;
+    await page.setContent(html,{waitUntil:'domcontentloaded'});
+    const pdfOptions = { format: 'a4', printBackground: true};
+    const pdfBuffer = await page.pdf(pdfOptions);
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+    await browser.close();
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+    res.set('Content-Type', 'application/pdf'); 
+    res.send(pdfBuffer);
+    console.log('Screenshot taken');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
